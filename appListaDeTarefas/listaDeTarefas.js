9583d94ef31b5ae77995b52152578a763b0b228d
Rodrigo-M-Brazao <rodrigombrazao@hotmail.com>
2022-10-21 22:08:00 -0300

    filtro e localStorage

--- appListaDeTarefas/listaDeTarefas.js
+++ appListaDeTarefas/listaDeTarefas.js
@@ -1,15 +1,19 @@
 
-const tarefa = document.getElementById('tarefa');
+const entradaTarefa = document.getElementById('tarefa');
 const btnAdd = document.querySelector('.btn');
 const lista = document.querySelector('.collection');
 const filtro = document.getElementById('filtro');
 const btnLimpar = document.querySelector('.limpar-tarefas');
+
+let tarefas = [];
 
 function carregaMonitoresDeEventos(){
     //Adiciona tarefa
     btnAdd.addEventListener('click', addTarefa);
     //Evento para fazer o x funcionar (apagar uma unica tarefa)
     lista.addEventListener('click', apagaTarefa);
+    btnLimpar.addEventListener('click', apagaTarefas);
+    filtro.addEventListener('keyup', filtraTarefa);
 }
 carregaMonitoresDeEventos();
 
@@ -27,15 +31,16 @@ function addTarefa(evento){
     evento.preventDefault();
     
     //Valida a entrada de dados
-    if(tarefa.value === ''){
+    if(entradaTarefa.value === ''){
         return alert('Escreva uma tarefa');
     }
-
+    //Grava a tarefa no localStorage
+    gravaTarefa(entradaTarefa.value);
     // Criando uma nova li
     const li = document.createElement('li');
     li.className = 'collection-item';
 
-    li.appendChild(document.createTextNode(tarefa.value));
+    li.appendChild(document.createTextNode(entradaTarefa.value));
 
     //Cria o a onde vai ficar o x para apagar a tarefa
     const a = document.createElement('a');
@@ -50,5 +55,52 @@ function addTarefa(evento){
     li.appendChild(a);
     lista.appendChild(li);
 
-    tarefa.value = '';
+    
+    entradaTarefa.value = '';
+
+    
+}
+
+// Apaga todas as tarefas 
+function apagaTarefas(evento){
+    evento.preventDefault();
+
+    lista.innerHTML = '';
+    //Solução 1
+    // let arrayLista = Array.from(lista.children);
+    // arrayLista.forEach(function(elemento){
+    //     elemento.remove();
+    // });
+}
+
+// Filtra as tarefas
+function filtraTarefa(evento){
+    const procurado = evento.target.value.toLowerCase();
+    const tarefas = Array.from(document.querySelectorAll('.collection-item'));
+
+    //indexOf retorna -1 caso não encontre o valor, se o valor for encontrado ele indica a posição do valor na string
+    tarefas.forEach(function(tarefa){
+
+        let tarefaLowerCase = tarefa.textContent.toLowerCase();
+
+        if(tarefaLowerCase.includes(procurado)){
+            tarefa.style.display = 'block';
+        }
+        else{
+            tarefa.style.display = 'none';
+        }
+    })
+}
+
+
+
+function gravaTarefa(tarefa){
+   
+    let tarefaDoStorage = localStorage.getItem('tarefas');
+    if(tarefaDoStorage != null){
+        tarefas = JSON.parse(tarefaDoStorage);
+    }
+    tarefas.push(tarefa);
+
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
 }
